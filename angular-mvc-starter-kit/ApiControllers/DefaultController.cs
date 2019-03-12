ad6a191a5c18c42a2a9e22587dcac0d582abe48f
aviel <avielbt@gmail.com>
2019-03-12 17:01:59 +0200

    first commit

---
+++ angular-mvc-starter-kit/ApiControllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace angular_mvc_starter_kit.ApiControllers
+{
+    [RoutePrefix("api")]
+    public class DefaultController : ApiController
+    {
+        
+        [Route("text")]
+        [HttpGet]
+        public string Text()
+        {
+            return "Got some text!";
+        }
+
+        
+    }
+}
--- angular-mvc-starter-kit/App_Start/RouteConfig.cs
+++ angular-mvc-starter-kit/App_Start/RouteConfig.cs
@@ -11,12 +11,10 @@ namespace angular_mvc_starter_kit
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{*catchall}",
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
