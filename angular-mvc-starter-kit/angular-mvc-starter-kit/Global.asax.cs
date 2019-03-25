4f35ed93775b96dc5db6a7262e7a9cbb56465d02
aviel <avielbt@gmail.com>
2019-03-26 00:06:22 +0200

    add json formatting defaults

--- angular-mvc-starter-kit/angular-mvc-starter-kit/Global.asax.cs
+++ angular-mvc-starter-kit/angular-mvc-starter-kit/Global.asax.cs
@@ -7,6 +7,9 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace angular_mvc_starter_kit
 {
@@ -14,6 +17,8 @@ namespace angular_mvc_starter_kit
     {
         void Application_Start(object sender, EventArgs e)
         {
+            InitializeJsonConfiguration();
+
             MvcHandler.DisableMvcResponseHeader = true;
             GlobalConfiguration.Configure(WebApiConfig.Register); // WebAPI
 
@@ -21,7 +26,24 @@ namespace angular_mvc_starter_kit
             MvcConfig.RegisterRoutes(RouteTable.Routes);
             MvcConfig.RegisterFilters(GlobalFilters.Filters);
 
-            
+
         }
+
+        private static void InitializeJsonConfiguration()
+        {
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings = new JsonSerializerSettings
+            {
+#if DEBUG
+                Formatting = Formatting.Indented,
+#else
+                Formatting = Formatting.None,
+#endif
+                TypeNameHandling = TypeNameHandling.None,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            };
+            formatter.SerializerSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+        }
+
     }
 }
